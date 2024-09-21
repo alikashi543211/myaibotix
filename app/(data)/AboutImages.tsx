ceb656459093316a2c2e4acbffb0c4e71eb3d87b
alikashi543211 <alikashi54321@gmail.com>
2024-09-21 19:13:40 +0500

    image extensions captialize

--- app/(data)/AboutImages.tsx
+++ app/(data)/AboutImages.tsx
@@ -1,5 +1,5 @@
 export default
 {
-    'image_1': "/front-assets/images/about-myaibotix-04.png",
-    'image_2': "/front-assets/images/about-myaibotix-02.png"
+    'image_1': "/front-assets/images/about-myaibotix-04.PNG",
+    'image_2': "/front-assets/images/about-myaibotix-02.PNG"
 }
