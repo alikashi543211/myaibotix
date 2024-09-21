10d220c63ebde5f1f686a7704cfcee8d8f870c92
alikashi543211 <alikashi54321@gmail.com>
2024-09-21 20:20:36 +0500

    copy editor data

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 import { Copy } from 'lucide-react';
@@ -9,6 +9,7 @@ export interface PROPS{
 }
 function OutputSection({aiOutput}:PROPS) {
   const editorRef:any = useRef()
+  const [copied, setCopied] = useState(false);
   useEffect(() => {
     const editorInstance = editorRef.current.getInstance()
     editorInstance.setMarkdown(aiOutput);
@@ -16,11 +17,15 @@ function OutputSection({aiOutput}:PROPS) {
 
   // Copy the current content of the editor to the clipboard
   const handleCopy = () => {
+    setCopied(true);
     const editorInstance = editorRef.current?.getInstance();
     if (editorInstance) {
       const content = editorInstance.getMarkdown();
       navigator.clipboard.writeText(content);
-      alert("Content copied to clipboard!");
+      
+      setTimeout(() => {
+        setCopied(false)
+      }, 700);
     }
   };
   return (
@@ -29,8 +34,12 @@ function OutputSection({aiOutput}:PROPS) {
         <h2 className='font-bold text-lg'>
           Your Result
         </h2>
-        <Button onClick={handleCopy} className='flex gap-2'>
-          <Copy className='w-4 h-4' />Copy
+        <Button 
+          onClick={handleCopy} 
+          className={`flex gap-2 ${copied ? '!bg-green-600' : ''}`}
+        >
+          <Copy className="w-4 h-4" />
+          <span>{copied ? 'Copied' : 'Copy'}</span>
         </Button>
       </div>
       <Editor
